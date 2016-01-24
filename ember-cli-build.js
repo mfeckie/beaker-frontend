bfca784969e81b0c06f3f86ad1ee6ea3040ee7e0
Martin Feckie <mfeckie@gmail.com>
2016-01-24 08:48:16 +0800

    Add highcharts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,9 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/highcharts/highcharts.js');
+  app.import('bower_components/highcharts/themes/dark-blue.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
